8a04a780db5a6032b4c23bd6aa52719d96c6a904
Sunhuan <1685880687@qq.com>
2025-01-13 18:00:31 +0800

    修复资源释放时的bug

--- acl_net.py
+++ acl_net.py
@@ -1,79 +1,111 @@
 import acl
 import numpy as np
   
-class ACL_inference(object):
-    def __init__(self, device_id):
-        self.device_id = device_id
-        acl.init()
-        acl.rt.set_device(self.device_id)
-        self.context, _ = acl.rt.create_context(self.device_id)
-        self.ACL_MEMCPY_HOST_TO_DEVICE = 1
-        self.ACL_MEMCPY_DEVICE_TO_HOST = 2
-        self.ACL_MEM_MALLOC_HUGE_ONLY = 2
-        self.model_id = None
-        self.model_desc = None
-        self.load_input_dataset = None
-        self.load_output_dataset = None
-        self.input_data = []
-        self.output_data = []
- 
-    def init(self, model_path):
-        self.model_id, _ = acl.mdl.load_from_file(model_path)
+class ACL_Net(object):
+
+    def __init__(self, model_path):
+        # 初始化函数
+        self.device_id = 0
+
+        # step1: 初始化
+        ret = acl.init()
+        # 指定运算的Device
+        ret = acl.rt.set_device(self.device_id)
+
+        # step2: 加载模型，本示例为ResNet-50模型
+        # 加载离线模型文件，返回标识模型的ID
+        self.model_id, ret = acl.mdl.load_from_file(model_path)
+        # 创建空白模型描述信息，获取模型描述信息的指针地址
         self.model_desc = acl.mdl.create_desc()
+        # 通过模型的ID，将模型的描述信息填充到model_desc
         ret = acl.mdl.get_desc(self.model_desc, self.model_id)
-        self.gen_input_dataset()
-        self.gen_output_dataset()
- 
-    def gen_output_dataset(self):
-        self.load_output_dataset = acl.mdl.create_dataset()
-        # 获取模型输出的数量。
-        output_size = acl.mdl.get_num_outputs(self.model_desc)
-        # 循环为每个输出申请内存，并将每个输出添加到aclmdlDataset类型的数据中。
-        for i in range(output_size):
-            buffer_size = acl.mdl.get_output_size_by_index(self.model_desc, i)
-            # 申请输出内存。
-            buffer, ret = acl.rt.malloc(buffer_size, self.ACL_MEM_MALLOC_HUGE_ONLY)
-            data = acl.create_data_buffer(buffer, buffer_size)
-            _, ret = acl.mdl.add_dataset_buffer(self.load_output_dataset, data)
-            self.output_data.append({"buffer": buffer, "size": buffer_size})
- 
-    def gen_input_dataset(self):
-        self.load_input_dataset = acl.mdl.create_dataset()
-        input_size = acl.mdl.get_num_inputs(self.model_desc)
-        print("input_size:",input_size)
-        for i in range(1):
-            buffer_size = acl.mdl.get_input_size_by_index(self.model_desc, i)
-            buffer, ret = acl.rt.malloc(buffer_size, self.ACL_MEM_MALLOC_HUGE_ONLY)
-            data = acl.create_data_buffer(buffer, buffer_size)
-            _, ret = acl.mdl.add_dataset_buffer(self.load_input_dataset, data)
-            self.input_data.append({"buffer": buffer, "size": buffer_size})
-            print("add to self.input_data, buffer",buffer,"size:",buffer_size)
- 
-    def process_output(self):
+
+        # step3：创建输入输出数据集
+        # 创建输入数据集
+        self.input_dataset, self.input_data = self.prepare_dataset('input')
+        # 创建输出数据集
+        self.output_dataset, self.output_data = self.prepare_dataset('output')
+
+    def prepare_dataset(self, io_type):
+        # 准备数据集
+        if io_type == "input":
+            # 获得模型输入的个数
+            io_num = acl.mdl.get_num_inputs(self.model_desc)
+            acl_mdl_get_size_by_index = acl.mdl.get_input_size_by_index
+        else:
+            # 获得模型输出的个数
+            io_num = acl.mdl.get_num_outputs(self.model_desc)
+            acl_mdl_get_size_by_index = acl.mdl.get_output_size_by_index
+        # 创建aclmdlDataset类型的数据，描述模型推理的输入。
+        dataset = acl.mdl.create_dataset()
+        datas = []
+        for i in range(io_num):
+            # 获取所需的buffer内存大小
+            buffer_size = acl_mdl_get_size_by_index(self.model_desc, i)
+            # 申请buffer内存
+            buffer, ret = acl.rt.malloc(buffer_size, ACL_MEM_MALLOC_HUGE_FIRST)
+            # 从内存创建buffer数据
+            data_buffer = acl.create_data_buffer(buffer, buffer_size)
+            # 将buffer数据添加到数据集
+            _, ret = acl.mdl.add_dataset_buffer(dataset, data_buffer)
+            datas.append({"buffer": buffer, "data": data_buffer, "size": buffer_size})
+        return dataset, datas
+
+    def forward(self, inputs):
+        # 执行推理任务
+        # 遍历所有输入，拷贝到对应的buffer内存中
+        print(type(acl))
+        input_num = len(inputs)
+        for i in range(input_num):
+            bytes_data = inputs[i].tobytes()
+            bytes_ptr = acl.util.bytes_to_ptr(bytes_data)
+            # 将图片数据从Host传输到Device。
+            ret = acl.rt.memcpy(self.input_data[i]["buffer"],  # 目标地址 device
+                                self.input_data[i]["size"],  # 目标地址大小
+                                bytes_ptr,  # 源地址 host
+                                len(bytes_data),  # 源地址大小
+                                ACL_MEMCPY_HOST_TO_DEVICE)  # 模式:从host到device
+        # 执行模型推理。
+        ret = acl.mdl.execute(self.model_id, self.input_dataset, self.output_dataset)
+        # 处理模型推理的输出数据，输出top5置信度的类别编号。
         inference_result = []
         for i, item in enumerate(self.output_data):
-            dims = acl.mdl.get_output_dims(self.model_desc, i)
-            shape = tuple(dims[i]["dims"])
             buffer_host, ret = acl.rt.malloc_host(self.output_data[i]["size"])
             # 将推理输出数据从Device传输到Host。
-            acl.rt.memcpy(buffer_host, self.output_data[i]["size"], self.output_data[i]["buffer"],
-                          self.output_data[i]["size"], self.ACL_MEMCPY_DEVICE_TO_HOST)
+            ret = acl.rt.memcpy(buffer_host,  # 目标地址 host
+                                self.output_data[i]["size"],  # 目标地址大小
+                                self.output_data[i]["buffer"],  # 源地址 device
+                                self.output_data[i]["size"],  # 源地址大小
+                                ACL_MEMCPY_DEVICE_TO_HOST)  # 模式：从device到host
+            # 从内存地址获取bytes对象
             bytes_out = acl.util.ptr_to_bytes(buffer_host, self.output_data[i]["size"])
-            data = np.frombuffer(bytes_out, dtype=np.float32).reshape(shape)
+            # 按照float32格式将数据转为numpy数组
+            data = np.frombuffer(bytes_out, dtype=np.float32)
             inference_result.append(data)
+            # 释放内存
+            ret = acl.rt.free_host(buffer_host)
+        print(type(acl))
+        print(self.model_desc)
+
         return inference_result
- 
-    def load_input_data(self, img):
-        bytes_data = img.tobytes()
-        np_ptr = acl.util.bytes_to_ptr(bytes_data)
-        # 将图片数据从Host传输到Device。
-        acl.rt.memcpy(self.input_data[0]["buffer"], self.input_data[0]["size"], np_ptr,
-                      self.input_data[0]["size"], self.ACL_MEMCPY_HOST_TO_DEVICE)
- 
-    def execute(self):
-        acl.mdl.execute(self.model_id, self.load_input_dataset, self.load_output_dataset)
- 
-    def destory(self):
-        acl.rt.destroy_context(self.context)
-        acl.rt.reset_device(self.device_id)
-        acl.finalize()
+
+    def __del__(self):
+        # 析构函数 按照初始化资源的相反顺序释放资源。
+        # 销毁输入输出数据集
+        #pass
+            
+        for dataset in [self.input_data, self.output_data]:
+            while dataset:
+                item = dataset.pop()
+                ret = acl.destroy_data_buffer(item["data"])  # 销毁buffer数据
+                ret = acl.rt.free(item["buffer"])  # 释放buffer内存
+        ret = acl.mdl.destroy_dataset(self.input_dataset)  # 销毁输入数据集
+        ret = acl.mdl.destroy_dataset(self.output_dataset)  # 销毁输出数据集
+        # 销毁模型描述
+        ret = acl.mdl.destroy_desc(self.model_desc)
+        # 卸载模型
+        ret = acl.mdl.unload(self.model_id)
+        # 释放device
+        ret = acl.rt.reset_device(self.device_id)
+        # acl去初始化
+        ret = acl.finalize()
